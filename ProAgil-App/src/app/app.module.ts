cb65d36417e9f5416d5488c2200635e6d5094cd8
marcosaraujo-dev <marcos.araso@hotmail.com>
2021-11-03 15:59:10 -0300

    Organização do Projeto Angular

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -2,26 +2,44 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { AppRoutingModule } from './app-routing.module';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { EventoService } from './_services/evento.service';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
+
+import { DateTimeFormatPipe } from './_helpers/DateTimeFormatPipe';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
       EventosComponent,
-      NavComponent
+      NavComponent,
+      DateTimeFormatPipe
    ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    AccordionModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    EventoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { EventoService } from '../_services/evento.service';
+import { Evento } from '../_models/Evento';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-eventos',
@@ -7,30 +10,39 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
-
-  _filtroLista: string = "";
-  get filtroLista(): string {
-    return this._filtroLista;
-  };
-  set filtroLista(value: string){
-    this._filtroLista = value;
-    this.eventosFiltrados = this.filtroLista ? this.filtrarEvento(this.filtroLista): this.eventos;
-  };
-
-  eventosFiltrados: any = [];
-  eventos: any = [];
+  eventosFiltrados: Evento[] = [];
+  eventos: Evento[] = [];
   imagemLargura: number = 50;
   imagemMargem: number = 2;
   mostrarImagem: boolean = false;
+  modalRef: BsModalRef | any;
+
+  _filtroLista: string = "";
+
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEvento(this.filtroLista): this.eventos;
+  }
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private eventoService: EventoService
+            , private modalService: BsModalService
+    ) { }
+
+
+  //Metodos para criar a modal
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 
   ngOnInit() {
     this.getEventos();
   }
 
-  filtrarEvento(filtrarPor: string): any{
+  filtrarEvento(filtrarPor: string): Evento[]{
     filtrarPor = filtrarPor.toLocaleLowerCase();
     return this.eventos.filter(
       (evento : any) => {
@@ -40,8 +52,11 @@ export class EventosComponent implements OnInit {
   }
 
   getEventos(){
-    this.http.get('http://localhost:5000/api/values').subscribe(
-      response => {this.eventos = response; this.eventosFiltrados = this.eventos;},
+    this.eventoService.getAllEvento().subscribe(
+      (_eventos) => {
+        this.eventos = _eventos;
+        this.eventosFiltrados = this.eventos;
+      },
       error => {console.log(error)}
     );
 
